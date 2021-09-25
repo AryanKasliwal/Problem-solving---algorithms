dc48ce1bc2317b5bf3d7037600af7ff29ff7fc3d
AryanKasliwal <70106055+AryanKasliwal@users.noreply.github.com>
2021-09-25 22:31:29 +0800

    Parasite test

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -268,7 +268,7 @@ class Room:
             return answer
 
 
-@app.route('/parasite', methods=['GET'])
+@app.route('/parasite', methods=['POST'])
 def evaluate_parasite():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
