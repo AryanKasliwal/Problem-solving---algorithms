72e8c5ff1bfb5913ad1f3ec78b22ef51d776214e
AryanKasliwal <70106055+AryanKasliwal@users.noreply.github.com>
2021-09-26 02:57:09 +0800

    Asteroid update

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -7,7 +7,6 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-
 @app.route('/asteroid', methods=['POST'])
 def evaluate_asteroid():
     data = request.get_json()
@@ -18,45 +17,35 @@ def evaluate_asteroid():
     for test in input_value:
         highest_score, highest_origin = int(), int()
         for i in range(len(test)):
-            astroids = {}
             if i == len(test) or i == 0:
-                if highest_score < 1:
-                    highest_score = 1
-                    highest_origin = i
-                continue
-            current_score = 1
-            lowest_origin, highest_origin = i - 1, i + 1
-            while highest_origin < len(test) and lowest_origin >= 0:
-                looping = False
-                while lowest_origin >= 0 and test[lowest_origin] == test[highest_origin]:
-                    try:
-                        astroids[test[lowest_origin]] += 1
-                    except:
-                        astroids[test[lowest_origin]] = 1
-                    lowest_origin -= 1
-                    looping = True
-                if looping:
-                    lowest_origin += 1
-                while highest_origin < len(test) and test[lowest_origin] == test[highest_origin]:
-                    astroids[test[lowest_origin]] += 1
-                    highest_origin += 1
-                if looping:
-                    lowest_origin -= 1
-                if test[lowest_origin + 1] in astroids.keys():
-                    num = astroids[test[lowest_origin + 1]]
-                    if num <= 6:
-                        current_score += num
-                    elif 7 <= num < 10:
-
-                        current_score += (num * 1.5)
-                    else:
-                        current_score += (num * 2)
-                    astroids[test[lowest_origin + 1]] = 0
-                if highest_origin < len(test) and lowest_origin >= 0 and test[lowest_origin] != test[highest_origin]:
-                    break
+                current_score = 1
+            else:
+                lower_origin, higher_origin, current_score, value = i - 1, i + 1, 1, 0
+                while higher_origin < len(test) and lower_origin >= 0:
+                    looping = False
+                    while higher_origin < len(test) and test[lower_origin] == test[higher_origin]:
+                        value += 1
+                        higher_origin += 1
+                        looping = True
+                    if looping == True:
+                        higher_origin -= 1
+                    while lower_origin >= 0 and test[lower_origin] == test[higher_origin]:
+                        value += 1
+                        lower_origin -= 1
+                    if looping == True:
+                        higher_origin += 1
+                    if test[lower_origin + 1] in astroids.keys():
+                        if value < 10 and value >= 7:
+                            value *= 1.5
+                        elif value >= 10:
+                            value *= 2
+                        current_score += value
+                        value = 0
+                    if lower_origin >= 0 and highest_origin < len(test) and test[higher_origin] != test[lower_origin]:
+                        break
             if current_score > highest_score:
-                highest_score = current_score
                 highest_origin = i
+                highest_score = current_score
         cur_result = {"input": test, "score": highest_score, "origin": highest_origin}
         answer.append(cur_result)
     logging.info("My result :{}".format(answer))
