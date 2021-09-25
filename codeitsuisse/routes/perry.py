868f3612f512de69aa7684eb6378546abad677a7
AryanKasliwal <70106055+AryanKasliwal@users.noreply.github.com>
2021-09-26 06:13:53 +0800

    perry update

--- codeitsuisse/routes/perry.py
+++ codeitsuisse/routes/perry.py
@@ -19,14 +19,9 @@ def fraction(p, q):
     return p, q
 
 
-def fact(n):
-    if (n <= 1):
-        return 1
-    return n * fact(n - 1)
-
-
 def nPr(n, r):
-    return math.floor(fact(n) / fact(n - r))
+    return math.factorial(n)/math.factorial(n - r)
+
 
 def processQuestions(questions, maxRating):
     # output_list = []
