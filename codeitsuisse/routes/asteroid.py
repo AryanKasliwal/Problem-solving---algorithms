9d6decd3572fb0400214e60042af990ff025394b
AryanKasliwal <70106055+AryanKasliwal@users.noreply.github.com>
2021-09-26 02:33:40 +0800

    asteroid update

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -17,19 +17,15 @@ def evaluate_asteroid():
 
     for test in input_value:
         highest_score, highest_origin = int(), int()
-
         for i in range(len(test)):
             astroids = {}
-
             if i == len(test) or i == 0:
                 if highest_score < 1:
                     highest_score = 1
                     highest_origin = i
                 continue
-
             current_score = 1
             lowest_origin, highest_origin = i - 1, i + 1
-
             while highest_origin < len(test) and lowest_origin >= 0:
                 looping = False
                 while lowest_origin >= 0 and test[lowest_origin] == test[highest_origin]:
@@ -41,32 +37,27 @@ def evaluate_asteroid():
                     looping = True
                 if looping:
                     lowest_origin += 1
-
                 while highest_origin < len(test) and test[lowest_origin] == test[highest_origin]:
                     astroids[test[lowest_origin]] += 1
                     highest_origin += 1
                 if looping:
                     lowest_origin -= 1
-
                 if test[lowest_origin + 1] in astroids.keys():
                     num = astroids[test[lowest_origin + 1]]
                     if num <= 6:
                         current_score += num
                     elif 7 <= num < 10:
+
                         current_score += (num * 1.5)
                     else:
                         current_score += (num * 2)
                     astroids[test[lowest_origin + 1]] = 0
-
-                if lowest_origin >= 0 and highest_origin < len(test) and test[lowest_origin] != test[highest_origin]:
+                if highest_origin < len(test) and lowest_origin >= 0 and test[lowest_origin] != test[highest_origin]:
                     break
-
             if current_score > highest_score:
                 highest_score = current_score
                 highest_origin = i
-
         cur_result = {"input": test, "score": highest_score, "origin": highest_origin}
         answer.append(cur_result)
-
     logging.info("My result :{}".format(answer))
     return json.dumps(answer)
