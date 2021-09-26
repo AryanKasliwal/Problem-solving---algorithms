d794268e34930b83b433dff5e56ec2f5f467b6e5
AryanKasliwal <70106055+AryanKasliwal@users.noreply.github.com>
2021-09-26 08:48:46 +0800

    a

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 def evaluate_decoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    result = {"answer" : [ 'w', 'y', 'z', 'p', 'l']}
+    result = {"answer" : [ 'w', 'y', 'h', 'p', 'l']}
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
