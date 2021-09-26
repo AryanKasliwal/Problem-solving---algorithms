f37500b9450b9d41e98d1e0f641f87fb52e065d3
AryanKasliwal <70106055+AryanKasliwal@users.noreply.github.com>
2021-09-26 08:30:53 +0800

    a

---
+++ codeitsuisse/routes/decoder.py
@@ -0,0 +1,19 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/decoder', methods=['POST'])
+def evaluate_decoder():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    result = {"answer" : []}
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
+
+
+
